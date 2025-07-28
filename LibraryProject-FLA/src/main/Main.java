9544e7ca8b2ba2d23a0d555f2ea116b8d0a6a69a
Lasercl <laserclauss2612@gmail.com>
2025-07-28 23:07:32 +0700

    add code to  implement javafx

--- LibraryProject-FLA/src/main/Main.java
+++ LibraryProject-FLA/src/main/Main.java
@@ -1038,8 +1038,6 @@ public class Main {
         }
 
         public static void main(String[] args) {
-//                new Main();
-//        	Application.launch(args);
 
         	Application.launch(HomeView.class,args);
 
